6f2a0997b1e5d00e114b5a73b6df3bed89276d78
ibbimadugu <Ibrahymmadugu6@gmail.com>
2025-04-12 15:29:56 +0100

    Quick fix

--- src/api.js
+++ src/api.js
@@ -1,6 +1,13 @@
 // src/api.js
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+// Use Vite-specific import.meta.env for dynamic base URL
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://p2pcarsharing-backend.onrender.com/api";
+
+const API = axios.create({
+  baseURL: BASE_URL,
+});
 
 export default API;
